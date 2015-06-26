9f2ececb1517b791065db0e1269e3b57cc901a89
Tony-cs1 <root@netvis1.(none)>
2015-06-26 16:26:56 -0400

    Update README and clean up tailer.js

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# cybervis
+# Cyber Defense Visualization
+
+### Description
+
+This wep application will be used to visualize the current network activity
+during cyber defense competitions. This is similar to what Norse has done
+with there [live threat map](map.ipviking.com). This application is to show
+what is going on during these cyber defense competitions. 
+
+### Dependencies
+- Argus
+- Node.js
+- Socket.io
+
+This is currently being tested on a CentOS 6.6 box.
+
+### About
+There are two parts to this project: the server-side and the front-end.
+This repo is being used to present the server-side code with some front-end
+to test. This is a project being done for my NCS 490 Network Engineering
+class at [SUNY Polytechnic Institute](http://sunypoly.edu). I intend to
+continue to improve this project after the class and would appreciate any
+suggestions and/or help especially with the front-end.
--- tailer.js
+++ tailer.js
@@ -1,10 +1,10 @@
-var filename = process.argv[2];
-var fs = require('fs')
-var express = require('express'),
+var fs = require('fs'),
+    express = require('express'),
     app = express(),
     http = require('http'),
     server = http.createServer(app),
     io = require('socket.io').listen(server);
+var argument = process.argv.slice(2, process.argv.length);
 var connected_users={};
 
 server.listen(80);
@@ -13,13 +13,12 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-var tail = require('child_process').spawn("ra",["-S", "127.0.0.1", "-n", "-s", "stime" ,"saddr", "sport", "daddr", "dport", "--", "ipv4"]);
+var argus = require('child_process').spawn("ra",["-S", "127.0.0.1", "-n", "-s", "stime" ,"saddr", "sport", "daddr", "dport", "--", "ipv4"]);
 io.sockets.on('connection', function (socket) {
     var socketId = socket.id;
     var clientIp = socket.request.connection.remoteAddress;
     console.log(clientIp);
-    tail.stdout.on('data', function(data){
-        // console.log('message!')
+    argus.stdout.on('data', function(data){
         socket.emit('news',{data: data.toString()})
     });
 });
