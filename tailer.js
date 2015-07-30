22a2b36cdc7d4f63336efff5ce610f82bf95571d
Tony-cs1 <root@netvis1.(none)>
2015-07-30 00:08:35 -0400

    Clean up files

--- tailer.js
+++ tailer.js
@@ -5,17 +5,17 @@ var fs = require('fs'),
     server = http.createServer(app),
     io = require('socket.io').listen(server);
 var argument = process.argv.slice(2, process.argv.length);
-var connected_users={};
 
+// Port to listen on
 server.listen(80);
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+// launch the argus client with only the columns we want
 var argus = require('child_process').spawn("ra",["-S", "127.0.0.1", "-n", "-s", "stime" ,"saddr", "sport", "daddr", "dport", "--", "ipv4"]);
 io.sockets.on('connection', function (socket) {
-    var socketId = socket.id;
     var clientIp = socket.request.connection.remoteAddress;
     console.log(clientIp);
     argus.stdout.on('data', function(data){
